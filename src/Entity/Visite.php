77d0773aaae0cf6872bfdf31fe57d35dbdcfc353
cathyLG <catherine.x.sart@gmail.com>
2021-12-29 10:53:58 +0100

    Phase 6 : ORM (création de la BDD et de la table 'visite')

---
+++ src/Entity/Visite.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VisiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VisiteRepository::class)
+ */
+class Visite
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $pays;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $datecreation;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $avis;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $tempsmin;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $tempsmax;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(string $pays): self
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    public function getDatecreation(): ?\DateTimeInterface
+    {
+        return $this->datecreation;
+    }
+
+    public function setDatecreation(?\DateTimeInterface $datecreation): self
+    {
+        $this->datecreation = $datecreation;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getAvis(): ?string
+    {
+        return $this->avis;
+    }
+
+    public function setAvis(?string $avis): self
+    {
+        $this->avis = $avis;
+
+        return $this;
+    }
+
+    public function getTempsmin(): ?int
+    {
+        return $this->tempsmin;
+    }
+
+    public function setTempsmin(?int $tempsmin): self
+    {
+        $this->tempsmin = $tempsmin;
+
+        return $this;
+    }
+
+    public function getTempsmax(): ?int
+    {
+        return $this->tempsmax;
+    }
+
+    public function setTempsmax(?int $tempsmax): self
+    {
+        $this->tempsmax = $tempsmax;
+
+        return $this;
+    }
+}
