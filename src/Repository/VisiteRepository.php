c9699a6d9bd52d824a65ea9e791ac5f7ec21f7f1
cathyLG <catherine.x.sart@gmail.com>
2021-12-29 16:18:04 +0100

    Phase 9 : ORM (tri des enregistrements)

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -12,39 +12,51 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Visite[]    findAll()
  * @method Visite[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class VisiteRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
+class VisiteRepository extends ServiceEntityRepository {
+
+    public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, Visite::class);
+    }
+
+    /**
+     * fonction retroune les enregistrement trié en fonction de champ en paramètre
+     * @param type $champ
+     * @param type $ordre
+     * @return Visite[]
+     */
+    public function findAllOrderBy($champ, $ordre): array {
+        return $this->createQueryBuilder('v')
+        ->orderBy('v.'.$champ, $ordre)
+        ->getQuery()
+        ->getResult();
     }
 
     // /**
     //  * @return Visite[] Returns an array of Visite objects
     //  */
     /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('v')
-            ->andWhere('v.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('v.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+      public function findByExampleField($value)
+      {
+      return $this->createQueryBuilder('v')
+      ->andWhere('v.exampleField = :val')
+      ->setParameter('val', $value)
+      ->orderBy('v.id', 'ASC')
+      ->setMaxResults(10)
+      ->getQuery()
+      ->getResult()
+      ;
+      }
+     */
 
     /*
-    public function findOneBySomeField($value): ?Visite
-    {
-        return $this->createQueryBuilder('v')
-            ->andWhere('v.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+      public function findOneBySomeField($value): ?Visite
+      {
+      return $this->createQueryBuilder('v')
+      ->andWhere('v.exampleField = :val')
+      ->setParameter('val', $value)
+      ->getQuery()
+      ->getOneOrNullResult()
+      ;
+      }
+     */
 }
