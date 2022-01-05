e01a21b017f0241dcc6f50a0dabcdee3e46fec80
cathyLG <catherine.x.sart@gmail.com>
2022-01-05 18:44:13 +0100

    Phase 19 : gestion des images

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -3,21 +3,41 @@
 namespace App\Entity;
 
 use App\Repository\VisiteRepository;
+use DateTimeImmutable;
+use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=VisiteRepository::class)
+ * @Vich\Uploadable
  */
-class Visite
-{
+class Visite {
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @Vich\UploadableField(mapping="visites", fileNameProperty="imageName")
+     * @Assert\Image(mimeTypes="image/jpeg")
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     */
+    private $imageName;
 
     /**
      * @ORM\Column(type="string", length=50)
@@ -59,100 +79,88 @@ class Visite
      */
     private $environnements;
 
-    public function __construct()
-    {
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $updated_at;
+
+    public function __construct() {
         $this->environnements = new ArrayCollection();
     }
 
-    public function getId(): ?int
-    {
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getVille(): ?string
-    {
+    public function getVille(): ?string {
         return $this->ville;
     }
 
-    public function setVille(string $ville): self
-    {
+    public function setVille(string $ville): self {
         $this->ville = $ville;
 
         return $this;
     }
 
-    public function getPays(): ?string
-    {
+    public function getPays(): ?string {
         return $this->pays;
     }
 
-    public function setPays(string $pays): self
-    {
+    public function setPays(string $pays): self {
         $this->pays = $pays;
 
         return $this;
     }
 
-    public function getDatecreation(): ?\DateTimeInterface
-    {
+    public function getDatecreation(): ?DateTimeInterface {
         return $this->datecreation;
     }
-    
-    public function getDatecreationString(): string
-    {
+
+    public function getDatecreationString(): string {
         return $this->datecreation->format('d/m/Y');
     }
 
-    public function setDatecreation(?\DateTimeInterface $datecreation): self
-    {
+    public function setDatecreation(?DateTimeInterface $datecreation): self {
         $this->datecreation = $datecreation;
 
         return $this;
     }
 
-    public function getNote(): ?int
-    {
+    public function getNote(): ?int {
         return $this->note;
     }
 
-    public function setNote(?int $note): self
-    {
+    public function setNote(?int $note): self {
         $this->note = $note;
 
         return $this;
     }
 
-    public function getAvis(): ?string
-    {
+    public function getAvis(): ?string {
         return $this->avis;
     }
 
-    public function setAvis(?string $avis): self
-    {
+    public function setAvis(?string $avis): self {
         $this->avis = $avis;
 
         return $this;
     }
 
-    public function getTempsmin(): ?int
-    {
+    public function getTempsmin(): ?int {
         return $this->tempsmin;
     }
 
-    public function setTempsmin(?int $tempsmin): self
-    {
+    public function setTempsmin(?int $tempsmin): self {
         $this->tempsmin = $tempsmin;
 
         return $this;
     }
 
-    public function getTempsmax(): ?int
-    {
+    public function getTempsmax(): ?int {
         return $this->tempsmax;
     }
 
-    public function setTempsmax(?int $tempsmax): self
-    {
+    public function setTempsmax(?int $tempsmax): self {
         $this->tempsmax = $tempsmax;
 
         return $this;
@@ -161,13 +169,11 @@ class Visite
     /**
      * @return Collection|Environnement[]
      */
-    public function getEnvironnements(): Collection
-    {
+    public function getEnvironnements(): Collection {
         return $this->environnements;
     }
 
-    public function addEnvironnement(Environnement $environnement): self
-    {
+    public function addEnvironnement(Environnement $environnement): self {
         if (!$this->environnements->contains($environnement)) {
             $this->environnements[] = $environnement;
         }
@@ -175,10 +181,41 @@ class Visite
         return $this;
     }
 
-    public function removeEnvironnement(Environnement $environnement): self
-    {
+    public function removeEnvironnement(Environnement $environnement): self {
         $this->environnements->removeElement($environnement);
 
         return $this;
     }
+
+    function getImageFile(): ?File {
+        return $this->imageFile;
+    }
+
+    function getImageName(): ?string {
+        return $this->imageName;
+    }
+
+    function setImageFile(?File $imageFile) {
+        $this->imageFile = $imageFile;
+        if ($this->imageFile instanceof UploadedFile) {
+            $this->updated_at = new \DateTime('now');
+        }
+        return $this;
+    }
+
+    function setImageName(?string $imageName) {
+        $this->imageName = $imageName;
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?DateTimeImmutable {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?DateTimeImmutable $updated_at): self {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
 }
