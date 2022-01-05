7745f966f0bc393fa739529d8344fea540901c54
cathyLG <catherine.x.sart@gmail.com>
2022-01-05 10:51:39 +0100

    Phase 14 : ORM (ajout d'un enregistrement)

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -70,13 +70,34 @@ class AdminVoyagesController extends AbstractController {
      */
     public function edit(Visite $visite, Request $request): Response {
         $formVisite = $this->createForm(VisiteType::class, $visite);
-        
+
         $formVisite->handleRequest($request);
-        if($formVisite->isSubmitted() && $formVisite->isValid()){
+        if ($formVisite->isSubmitted() && $formVisite->isValid()) {
             $this->om->flush();
             return $this->redirectToRoute('admin.voyages');
         }
         return $this->render("admin/admin.voyage.edit.html.twig", [
+                    'visite' => $visite,
+                    'formvisite' => $formVisite->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/ajout", name="admin.voyage.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response {
+        $visite = new Visite();
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+
+        $formVisite->handleRequest($request);
+        if ($formVisite->isSubmitted() && $formVisite->isValid()) {
+            $this->om->persist($visite);
+            $this->om->flush();
+            return $this->redirectToRoute('admin.voyages');
+        }
+        return $this->render("admin/admin.voyage.ajout.html.twig", [
                     'visite' => $visite,
                     'formvisite' => $formVisite->createView()
         ]);
